8f5fc59dde8a37fd7aa273376459c9889f46fd69
pakarlapudi sai krishna <psaikrishna787@gmail.com>
2026-07-28 07:16:59 +0530

    Add files via upload

---
+++ Day01/largest-of -three-numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, y, z;
+
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &x, &y, &z);
+
+    if (x > y && x > z)
+    {
+        printf("%d is the largest number.", x);
+    }
+    else if (y > x && y > z)
+    {
+        printf("%d is the largest number.", y);
+    }
+    else
+    {
+        printf("%d is the largest number.", z);
+    }
+
+    return 0;
+}
+
---
+++ Day01/prime-numbers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, i, flag = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (n <= 1)
+    {
+        flag = 0;
+    }
+
+    for (i = 2; i < n; i++)
+    {
+        if (n % i == 0)
+        {
+            flag = 0;
+            break;
+        }
+    }
+
+    if (flag == 1)
+        printf("Prime Number");
+    else
+        printf("Not Prime Number");
+
+    return 0;
+}
